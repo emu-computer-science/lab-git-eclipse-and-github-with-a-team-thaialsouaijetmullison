915b9099f3addd01896bbf2aebdffe06d236f03d
Isaac Mullison <isaacmullison@gmail.com>
2023-10-04 18:42:23 -0400

    add driver.java

---
+++ src/edu/emich/cosc381/gp1/Driver.java
@@ -0,0 +1,10 @@
+package edu.emich.cosc381.gp1;
+
+public class Driver {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Our team is just getting started....");
+	}
+
+}
