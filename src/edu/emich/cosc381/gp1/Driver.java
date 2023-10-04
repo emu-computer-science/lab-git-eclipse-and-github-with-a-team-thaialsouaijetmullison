3fd25747bd118c85a7443f7931d58c289dbb0989
Isaac Mullison <isaacmullison@gmail.com>
2023-10-04 18:46:40 -0400

    adding glad class

--- src/edu/emich/cosc381/gp1/Driver.java
+++ src/edu/emich/cosc381/gp1/Driver.java
@@ -5,6 +5,7 @@ public class Driver {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Our team is just getting started....");
+		Glad.glad();
 	}
 
 }
